8b0b5edca54a3f6b5ea695797f105e29397f1085
kacper-stasiak <kapsi2003@wp.pl>
2025-03-13 18:46:20 +0100

    Work in progress

--- Zadanie_1/main.py
+++ Zadanie_1/main.py
@@ -1,46 +1,114 @@
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
 import numpy as np
 
 from noises import *
-import matplotlib.pyplot as plt
 
-# x, y = uni_dist_noise(10, 0, 5)
+# #Sygnały ciągłe
+
+# A = 10  # amplituda szumu
+# t1 = 0  # początek przedziału
+# t2 = 5  # koniec przedziału
+# f = 1000  # częstotliwość próbkowania
+#
+# x = np.linspace(t1, t2, int(f * (t2 - t1)))
+# y = np.array([uniform_dist_noise_function(t, A) for t in x])
+
 # x, y = normal_dist_noise(2, 0, 100)
 # x, y = sin_signal(10, 6.28, 0, 10)
 # x, y = half_wave_rectified_sin_signal(1, 4, 0, 10)
 # x, y = full_wave_rectified_sin_signal(1, 5, 2, 7)
 # x, y = rectangular_signal(1, 2, 0, 10, 0.5)
+
+A = 1  # amplituda
+T = 2  # okres
+t1 = 0  # początek przedziału
+t2 = 10  # koniec przedziału
+kw = 0.5  # współczynnik szerokości prostokąta
+f = 1000  # częstotliwość próbkowania
+x = np.linspace(t1, t2, int(f * (t2 - t1)))
+y = np.array([rectangular_signal_function(t, A, T, kw, t1) for t in x])
+
 # x, y = rectangular_symmetrical_signal(1, 2, 0, 10, 0.5)
-x, y = triangle_signal(1, 2, 0, 10, 0.5)
-print(type(x))
-print(type(y))
-
-
+# x, y = triangle_signal(1, 2, 0, 10, 0.5)
 # x, y = unit_jump(5, -10, 10, 0)
+
+# A = 2  # amplituda
+# t1 = -10  # początek przedziału
+# t2 = 10  # koniec przedziału
+# ts = 0  # czas skoku
+# f = 1000  # częstotliwość próbkowania
+#
+# x = np.linspace(t1, t2, int(f * (t2 - t1)))
+# y = np.array([unit_jump_function(t, A, ts) for t in x])
+
+# #Sygnały dyskretne
 # x, y = unit_impulse(1, 0, 10, 5, 5)
+# x, y = impulse_noise(1, 0, 10, 5, 1)
+
+# A = 1
+# t1 = 0
+# d = 10
+# f = 5
+# p = 0.5
+#
+# x = np.arange(t1, t1+d, 1/f)
+# y = np.array([impulse_noise_function(t, A, p) for t in x])
 
 
-# plt.plot(x, y, color = "red")
+##Wartość średnia
+
+# #Dla ciągłych
+# integral = quad(unit_jump_function, t1, t2, args=(A, ts), limit = 1000)[0]
+# mean_value = integral / (t2 - t1)
+# print(mean_value)
+
+##Dla dyskretnych
+# mean_value = np.sum(y) / len(y)
+# print(mean_value)
+
+
+
+# #Wartość średnia bezwzgledna
+
+# integral = quad(rectangular_signal_function, t1, t2, args=(A, T, kw, t1), limit = 1000)[0]
+# mean_value = np.abs(integral / (t2 - t1))
+# print(mean_value)
+
+#Dla dyskretnych
+# mean_value = np.abs(np.sum(y) / len(y))
+# print(mean_value)
+
+# with open("plik.bin", "wb") as file:
+#     x.tofile(file)
+#     y.tofile(file)
+
+#Moc średnia sygnału:
+#Dla ciągłych
+# integral = quad(rectangular_signal_function, t1, t2, args=(A,T, kw, t1), limit = 1000)[0]
+# power_value = np.power(integral / (t2 - t1), 2)
+# print(power_value)
+
+#Dla dyskretnych
+# power_value = np.power(np.sum(y) / len(y), 2)
+# print(power_value)
+
+# with open("plik.bin", "rb") as file:
+#     data = np.fromfile(file, dtype = np.float64)
+#
+#     n =  int(len(data)/2)
+#
+#     x = data[:n]
+#     y = data[n:]
+
+
+# Oblicznaie parametrów
+
+
+
+
+plt.plot(x, y, color = "red")
 # plt.scatter(x, y, color = "red", marker = "s", s=10)
-# plt.plot(x, np.zeros_like(x), color = "blue", linestyle= "dashed")
-# plt.xticks(np.arange(min(x), max(x)+1, 1))
-# plt.show()
-
-def plot_signal_and_histogram(x, y, bins=50):
-    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
-
-    # Wykres sygnału
-    axs[0].plot(x, y)
-    axs[0].set_title(f'Sygnał')
-    axs[0].set_xlabel('Czas [s]')
-    axs[0].set_ylabel('Amplituda')
-
-    # Histogram
-    axs[1].hist(y, bins=bins, edgecolor='black', alpha=0.7)
-    axs[1].set_title(f'Histogram')
-    axs[1].set_xlabel('Wartości')
-    axs[1].set_ylabel('Liczność')
-
-    plt.show()
-
-
-plot_signal_and_histogram(x, y)
+plt.plot(x, np.zeros_like(x), color = "blue", linestyle= "dashed")
+plt.xticks(np.arange(min(x), max(x)+1, 1))
+plt.show()
--- Zadanie_1/noises.py
+++ Zadanie_1/noises.py
@@ -1,105 +1,174 @@
 import numpy as np
-def uni_dist_noise(A, t1, d):
-    f = 1000
-    x = np.linspace(t1, t1+d, int(f * d))
-    y = np.random.uniform(low=-A, high=A, size = len(x))
-    return x, y
+# def uni_dist_noise(A, t1, d):
+#     f = 1000
+#     x = np.linspace(t1, t1+d, int(f * d))
+#     y = np.random.uniform(low=-A, high=A, size = len(x))
+#     return x, y
 
-def normal_dist_noise(A, t1, d):
-    f = 1000
-    x = np.linspace(t1, t1+d, int(f * d))
-    y = A * np.random.normal(loc=0, scale=1, size=len(x))
-    return x, y
+def uniform_dist_noise_function(t, A):
+    return np.random.uniform(-A, A)
 
-def sin_signal(A, T, t1, d):
-    f = 1000
-    x = np.linspace(t1, t1+d, int(f * d))
-    y = A * np.sin((2*np.pi / T) * (x - t1))
-    return x, y
+# def normal_dist_noise(A, t1, d):
+#     f = 1000
+#     x = np.linspace(t1, t1+d, int(f * d))
+#     y = A * np.random.normal(loc=0, scale=1, size=len(x))
+#     return x, y
 
-def half_wave_rectified_sin_signal(A, T, t1, d):
-    f = 1000
-    x = np.linspace(t1, t1+d, int(f * d))
-    y = 0.5 * A * ( np.sin((2*np.pi / T) * (x - t1)) + np.abs(np.sin((2* np.pi / T) * (x - t1))))
-    return x, y
+def normal_dist_noise_function(t, A):
+    return A * np.random.normal(loc=0, scale=1)
 
-def full_wave_rectified_sin_signal(A, T, t1, d):
-    f = 1000
-    x = np.linspace(t1, t1+d, int(f * d))
-    y = 0.5 * A * ( np.abs(np.sin((2* np.pi / T) * (x - t1))))
-    return x, y
+# def sin_signal(A, T, t1, d):
+#     f = 1000
+#     x = np.linspace(t1, t1+d, int(f * d))
+#     y = A * np.sin((2*np.pi / T) * (x - t1))
+#     return x, y
 
-def rectangular_signal(A, T, t1, d, kw):
-    f = 1000
-    x = np.linspace(t1, t1+d, int(f * d))
-    y = np.zeros_like(x)
+def sin_signal_function(t, A, T):
+    return A * np.sin((2 * np.pi / T) * (t))
 
-    C = list(range(t1, t1 + d + 1))
-    for k in C:
-        start_time = k * T + t1
-        end_time = kw * T + k * T + t1
-        y[(x >= start_time) & (x < end_time)] = A
+# def half_wave_rectified_sin_signal(A, T, t1, d):
+#     f = 1000
+#     x = np.linspace(t1, t1+d, int(f * d))
+#     y = 0.5 * A * ( np.sin((2*np.pi / T) * (x - t1)) + np.abs(np.sin((2* np.pi / T) * (x - t1))))
+#     return x, y
 
-    return x, y
-
-def rectangular_symmetrical_signal(A, T, t1, d, kw):
-    f = 1000
-    x = np.linspace(t1, t1+d, int(f * d))
-    y = np.zeros_like(x)
-
-    C = list(range(t1, t1 + d + 1))
-    for k in C:
-        start_time = k * T + t1
-        end_time = kw * T + k * T + t1
-        y[(x >= start_time) & (x < end_time)] = A
-
-        start_time = kw * T + k * T + t1
-        end_time = T + k * T + t1
-        y[(x >= start_time) & (x < end_time)] = -A
-
-    return x, y
+def half_wave_rectified_sin_signal_function(t, A, T):
+    return 0.5 * A * (np.sin((2 * np.pi / T) * (t)) + np.abs(np.sin((2 * np.pi / T) * (t))))
 
 
-def triangle_signal(A, T, t1, d, kw):
-    f = 1000
-    x = np.linspace(t1, t1 + d, int(f * d))
-    y = np.zeros_like(x)
+# def full_wave_rectified_sin_signal(A, T, t1, d):
+#     f = 1000
+#     x = np.linspace(t1, t1+d, int(f * d))
+#     y = 0.5 * A * ( np.abs(np.sin((2* np.pi / T) * (x - t1))))
+#     return x, y
 
-    C = list(range(int(t1), int(t1 + d) + 1))
+def full_wave_rectified_sin_signal_function(t, A, T):
+    return 0.5 * A * np.abs(np.sin((2 * np.pi / T) * (t)))
 
-    for k in C:
-        start_time = k * T + t1
-        end_time = kw * T + k * T + t1
+# def rectangular_signal(A, T, t1, d, kw):
+#     f = 1000
+#     x = np.linspace(t1, t1+d, int(f * d))
+#     y = np.zeros_like(x)
+#
+#     C = list(range(t1, t1 + d + 1))
+#     for k in C:
+#         start_time = k * T + t1
+#         end_time = kw * T + k * T + t1
+#         y[(x >= start_time) & (x < end_time)] = A
+#
+#     return x, y
 
-        mask_rise = (x >= start_time) & (x < end_time)
-        y[mask_rise] = (A / (kw * T)) * (x[mask_rise] - k * T - t1)
+def rectangular_signal_function(t, A, T, kw, t1):
+    start_time = t1 + T * int(t // T)
+    end_time = start_time + kw * T
+    if start_time <= t < end_time:
+        return A
+    return 0
 
-        start_time = end_time
-        end_time = T + k * T + t1
-        mask_fall = (x >= start_time) & (x < end_time)
-        y[mask_fall] = (-A / (T * (1 - kw))) * (x[mask_fall] - k * T - t1) + A / (1 - kw)
+# def rectangular_symmetrical_signal(A, T, t1, d, kw):
+#     f = 1000
+#     x = np.linspace(t1, t1+d, int(f * d))
+#     y = np.zeros_like(x)
+#
+#     C = list(range(t1, t1 + d + 1))
+#     for k in C:
+#         start_time = k * T + t1
+#         end_time = kw * T + k * T + t1
+#         y[(x >= start_time) & (x < end_time)] = A
+#
+#         start_time = kw * T + k * T + t1
+#         end_time = T + k * T + t1
+#         y[(x >= start_time) & (x < end_time)] = -A
+#
+#     return x, y
 
-    return x, y
+def rectangular_symmetrical_signal_function(t, A, T, kw, t1):
+    start_time = t1 + T * int(t // T)
+    end_time = start_time + kw * T
+    if start_time <= t < end_time:
+        return A
+    start_time = end_time
+    end_time = start_time + (T - kw * T)
+    if start_time <= t < end_time:
+        return -A
+    return 0
 
-def unit_jump(A, t1, d, ts):
-    f = 1000
-    if t1 < 0:
-        x = np.linspace(t1, d, f * d)
-    else:
-        x = np.linspace(t1, t1 + d, int(f * d))
 
-    x = np.linspace(t1,  d, f * d)
-    y = np.zeros_like(x)
+# def triangle_signal(A, T, t1, d, kw):
+#     f = 1000
+#     x = np.linspace(t1, t1 + d, int(f * d))
+#     y = np.zeros_like(x)
+#
+#     C = list(range(int(t1), int(t1 + d) + 1))
+#
+#     for k in C:
+#         start_time = k * T + t1
+#         end_time = kw * T + k * T + t1
+#
+#         mask_rise = (x >= start_time) & (x < end_time)
+#         y[mask_rise] = (A / (kw * T)) * (x[mask_rise] - k * T - t1)
+#
+#         start_time = end_time
+#         end_time = T + k * T + t1
+#         mask_fall = (x >= start_time) & (x < end_time)
+#         y[mask_fall] = (-A / (T * (1 - kw))) * (x[mask_fall] - k * T - t1) + A / (1 - kw)
+#
+#     return x, y
 
-    y[x > ts] = A
-    y[x == ts] = 0.5*A
+def triangle_signal_function(t, A, T, kw, t1):
+    C = int((t - t1) // T)
+    start_time = t1 + T * C
+    end_time = start_time + kw * T
+    if start_time <= t < end_time:
+        return (A / (kw * T)) * (t - start_time)
+    start_time = end_time
+    end_time = start_time + (T - kw * T)
+    if start_time <= t < end_time:
+        return (-A / ((1 - kw) * T)) * (t - start_time) + A / (1 - kw)
+    return 0
 
-    return x, y
+# def unit_jump(A, t1, d, ts):
+#     f = 1000
+#     if t1 < 0:
+#         x = np.linspace(t1, d, f * d)
+#     else:
+#         x = np.linspace(t1, t1 + d, int(f * d))
+#
+#     x = np.linspace(t1,  d, f * d)
+#     y = np.zeros_like(x)
+#
+#     y[x > ts] = A
+#     y[x == ts] = 0.5*A
+#
+#     return x, y
 
-def unit_impulse(A, n1, l, ns, f):
-    x = np.arange(n1, n1 + l, 1/f)
-    y = np.zeros_like(x)
+def unit_jump_function(t, A, ts):
+    if t > ts:
+        return A
+    elif t == ts:
+        return 0.5 * A
+    return 0
 
-    y[x == ns] = A
+# def unit_impulse(A, n1, l, ns, f):
+#     x = np.arange(n1, n1 + l, 1/f)
+#     y = np.zeros_like(x)
+#
+#     y[x == ns] = A
+#
+#     return x, y
 
-    return x, y
+def unit_impulse_function(t, A, ns):
+    return A if t == ns else 0
+
+# def impulse_noise(A, t1, d, f, p):
+#     x = np.arange(t1, t1 + d, 1/f)
+#     y = np.zeros_like(x)
+#
+#     for i in range(len(x)):
+#         if p > np.random.rand():
+#             y[i] = A
+#
+#     return x, y
+
+def impulse_noise_function(t, A, p):
+    return A if p > np.random.rand() else 0
